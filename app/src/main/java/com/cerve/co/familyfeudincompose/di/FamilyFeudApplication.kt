54ed6e693396457e83d10c5cee6cef8aeb586fca
Revok <cerve.v55@gmail.com>
2022-10-25 16:32:26 -0400

    hilt dependency injection implementation.

---
+++ app/src/main/java/com/cerve/co/familyfeudincompose/di/FamilyFeudApplication.kt
@@ -0,0 +1,7 @@
+package com.cerve.co.familyfeudincompose.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FamilyFeudApplication : Application()
---
+++ app/src/main/java/com/cerve/co/familyfeudincompose/di/FamilyFeudModule.kt
@@ -0,0 +1,13 @@
+package com.cerve.co.familyfeudincompose.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+object FamilyFeudModule {
+
+
+
+}
