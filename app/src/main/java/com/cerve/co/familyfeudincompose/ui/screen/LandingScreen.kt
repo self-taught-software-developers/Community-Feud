e78217f26c39b5bbe941563c58eeb8c427178cf9
Revok <cerve.v55@gmail.com>
2022-10-26 07:43:59 -0400

    initial composable screens created.

---
+++ app/src/main/java/com/cerve/co/familyfeudincompose/ui/screen/LandingScreen.kt
@@ -0,0 +1,15 @@
+package com.cerve.co.familyfeudincompose.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LandingScreen() {
+
+}
+
+@Preview
+@Composable
+fun LandingScreenPreview() {
+
+}
