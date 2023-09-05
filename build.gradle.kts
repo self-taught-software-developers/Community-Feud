a4253acec3289552fdc49987b138efd16895deca
Revok <cerve.v55@gmail.com>
2023-09-05 14:47:50 -0400

    kmp(kotlin multiplatform) migration - version catalog(Dependency version file management) + build.gradle.kts plugin updates.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
+    kotlin("multiplatform").version("1.9.10").apply(false)
     id("com.android.application").version("8.0.0").apply(false)
     id("com.android.library").version("8.0.0").apply (false)
-    id("org.jetbrains.kotlin.android").version("1.9.10").apply (false)
+    id("org.jetbrains.compose").version("1.5.0").apply(false)
 }
