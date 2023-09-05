94e860feaf3a31d534ce88372d49c87b3c1bbb68
Revok <cerve.v55@gmail.com>
2023-09-05 17:06:46 -0400

    kmp(kotlin multiplatform) migration - version catalog(Dependency version file management) + build.gradle.kts plugin updates.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,64 +1,39 @@
 plugins {
+    kotlin("multiplatform")
     id ("com.android.application")
-    id ("org.jetbrains.kotlin.android")
-    id ("kotlin-kapt")
+    id("org.jetbrains.compose")
+}
+
+kotlin {
+    androidTarget()
+    sourceSets {
+        val androidMain by getting {
+            dependencies {
+                implementation(project(":shared"))
+            }
+        }
+    }
 }
 
 android {
     namespace = "com.cerve.co.familyfeudincompose"
-    compileSdk = 34
+    compileSdk = libs.versions.android.target.sdk.orNull?.toInt()
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
 
     defaultConfig {
         applicationId = "com.cerve.co.familyfeudincompose"
-        minSdk = 26
-        targetSdk = 33
+        minSdk = libs.versions.android.min.sdk.orNull?.toInt()
+        targetSdk = libs.versions.android.target.sdk.orNull?.toInt()
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-        }
-    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    buildFeatures {
-        compose = true
+    kotlin {
+        jvmToolchain(17)
     }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.2"
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-dependencies {
-
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation ("androidx.activity:activity-compose:1.7.2")
-    implementation ("androidx.compose.ui:ui:1.4.3")
-    implementation ("androidx.compose.ui:ui-tooling-preview:1.4.3")
-    implementation ("androidx.compose.material3:material3:1.2.0-alpha06")
-
-    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
-    implementation ("com.google.accompanist:accompanist-insets-ui:0.27.0")
-
-
-    implementation ("androidx.navigation:navigation-compose:2.7.1")
-    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
-
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:")
-    debugImplementation ("androidx.compose.ui:ui-tooling:1.2")
-    debugImplementation ("androidx.compose.ui:ui-test-manifest:1.2")
 }
